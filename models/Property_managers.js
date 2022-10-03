08a00f05659f1d950165192e2988daee531f801a
nelsonsakhile <nelsonsakhile@gmail.com>
2022-10-03 23:30:40 +0200

    Added Models

--- models/Property_managers.js
+++ models/Property_managers.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const properties_managers = new mongoose.Schema(
     { text: {type:String, required: true} },
-    { address: {type:String, required: true} },
+    { address: {type:String} },
 )
 
 module.exports = mongoose.model('properties_managers', properties_managers)
