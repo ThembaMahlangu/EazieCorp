d6f8f02efb3bbbebde916f705f0352bd4ef7dc2d
nelsonsakhile <nelsonsakhile@gmail.com>
2022-10-06 19:36:26 +0200

    jwt

---
+++ routers/property-list-section.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+router.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+router.get('/', function (req, res) {
+    
+    const properties = [{
+
+      ComponyName: 'Sakhile Properties',
+      Address: 'Jewel City, Johanessburg.'
+
+    },
+    {
+
+      ComponyName: 'Themba Properties',
+      Address: 'Jewel City, Johanessburg.'
+
+    }
+  ];
+
+  const listing = [{
+      type: 'For rent',
+      Building: 'Elevate',
+      text: 'A nice Bachelor',
+      price: 'R 3 069.00 pm',
+      size: '5000 SqM',
+      bedrooms: '1 Bedroom',
+      bath: '1 Bath'
+
+  },
+  {
+    type: 'For rent',
+    Building: 'Elevate',
+    text: 'A nice Bachelor',
+    price: 'R 3 069.00 pm',
+    size: '5000 SqM',
+    bedrooms: '1 Bedroom',
+    bath: '1 Bath'
+
+}
+];
+
+    res.render('property-list-section.ejs', {properties: properties, listing: listing})
+  });
+
+
+
+  module.exports = router;
