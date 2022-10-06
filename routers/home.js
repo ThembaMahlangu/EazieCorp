a7ad43d0060924844d12100eb8b14a3a37aa81c9
ThembaMahlangu <jvstraphael@gmail.com>
2022-10-06 12:47:16 +0200

    connecting the page

--- routers/home.js
+++ routers/home.js
@@ -44,5 +44,46 @@ router.get('/', function (req, res) {
     res.render('index.ejs', {properties: properties, listing: listing});
   });
 
+  router.get('/property-list-section', function (req, res) {
+    
+    const properties = [{
+
+      ComponyName: 'Sakhile Properties',
+      Address: 'Jewel City, Johanessburg.'
+
+    },
+    {
+
+      ComponyName: 'Themba Properties',
+      Address: 'Jewel City, Johanessburg.'
+
+    }
+  ];
+
+  const listing = [{
+      type: 'For rent',
+      Building: 'Elevate',
+      text: 'A nice Bachelor',
+      price: 'R 3 069.00 pm',
+      size: '5000 SqM',
+      bedrooms: '1 Bedroom',
+      bath: '1 Bath'
+
+  },
+  {
+    type: 'For rent',
+    Building: 'Elevate',
+    text: 'A nice Bachelor',
+    price: 'R 3 069.00 pm',
+    size: '5000 SqM',
+    bedrooms: '1 Bedroom',
+    bath: '1 Bath'
+
+}
+];
+
+    res.render('property-list-section.ejs', {properties: properties, listing: listing})
+  });
+
   
 module.exports = router;
