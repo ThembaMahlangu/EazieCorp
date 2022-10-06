ebbb1659219987a6e665a0fd0acbd7679468cb5b
nelsonsakhile <nelsonsakhile@gmail.com>
2022-10-06 19:33:05 +0200

    json web token

--- routers/home.js
+++ routers/home.js
@@ -51,46 +51,7 @@ router.get('/', function (req, res) {
     res.render('index.ejs', {properties: properties, listing: listing, new_testimonial : new_testimonial});
   });
 
-  router.get('/property-list-section', function (req, res) {
-    
-    const properties = [{
-
-      ComponyName: 'Sakhile Properties',
-      Address: 'Jewel City, Johanessburg.'
-
-    },
-    {
-
-      ComponyName: 'Themba Properties',
-      Address: 'Jewel City, Johanessburg.'
-
-    }
-  ];
-
-  const listing = [{
-      type: 'For rent',
-      Building: 'Elevate',
-      text: 'A nice Bachelor',
-      price: 'R 3 069.00 pm',
-      size: '5000 SqM',
-      bedrooms: '1 Bedroom',
-      bath: '1 Bath'
-
-  },
-  {
-    type: 'For rent',
-    Building: 'Elevate',
-    text: 'A nice Bachelor',
-    price: 'R 3 069.00 pm',
-    size: '5000 SqM',
-    bedrooms: '1 Bedroom',
-    bath: '1 Bath'
-
-}
-];
-
-    res.render('property-list-section.ejs', {properties: properties, listing: listing})
-  });
+ 
 
   
 module.exports = router;
